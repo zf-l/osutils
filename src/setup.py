f35da7ddda753e89e5c9424d0983e910f46adab3
unknown <zhifengx@ZHIFENGX-MOBL2.ccr.corp.intel.com>
2016-09-25 22:00:42 +0800

    Update python version settings

--- src/setup.py
+++ src/setup.py
@@ -1,6 +1,5 @@
-# Always prefer setuptools over distutils
+# encoding=utf-8
 from setuptools import setup, find_packages
-# To use a consistent encoding
 import codecs
 from os import path
 
@@ -15,52 +14,21 @@ setup(
     version='0.1.0',
     description='Windows Automaiton test utils lib',
     long_description=long_description,
-
     url='https://github.com/zf-l/osutils',
-
-    # Author details
     author='zf-l',
     author_email='zf.liu@foxmail.com',
-
-    # Choose your license
     license='MIT',
-
     classifiers=[
-        # How mature is this project? Common values are
-        #   3 - Alpha
-        #   4 - Beta
-        #   5 - Production/Stable
         'Development Status :: 3 - Alpha',
-
-        # Indicate who your project is intended for
         'Intended Audience :: Developers',
-        'Topic :: Software Development :: Build Tools',
-
-        # Pick your license as you wish (should match "license" above)
+        'Topic :: Software Development ::  Application Frameworks',
         'License :: OSI Approved :: MIT License',
-
-        # Specify the Python versions you support here. In particular, ensure
-        # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
+        #'Programming Language :: Python :: 3.5',
     ],
 
-    # What does your project relate to?
-    keywords='Windows autotest',
-
-    # You can just specify the packages manually here if your project is
-    # simple. Or you can use find_packages().
+    keywords='Windows autotest library',
     packages=find_packages(exclude=['contrib', 'docs', 'tests']),
-
-    # Alternatively, if you want to distribute just a my_module.py, uncomment
-    # this:
-    #   py_modules=["my_module"],
-
     install_requires=['pywin32', 'chardet', 'wmi'],
 
     # List additional groups of dependencies here (e.g. development
